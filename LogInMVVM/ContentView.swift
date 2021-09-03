f92d2adfca1cbae475dcd47ca333b481a5565233
Inpyo Hong <y2kpaulh@epiens.com>
2021-09-03 18:44:03 +0900

    Initial Commit

---
+++ LogInMVVM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LogInMVVM
+//
+//  Created by Inpyo Hong on 2021/09/03.
+//
+
+import SwiftUI
+
+/*
+ https://lawrey.medium.com/creating-a-swiftui-combine-alamofire-mvvm-login-example-df0bdc30ef25
+ */
+
+struct ContentView: View {
+    var body: some View {
+        LoginView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LogInMVVM/LogInMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogInMVVMApp.swift
+//  LogInMVVM
+//
+//  Created by Inpyo Hong on 2021/09/03.
+//
+
+import SwiftUI
+
+@main
+struct LogInMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
